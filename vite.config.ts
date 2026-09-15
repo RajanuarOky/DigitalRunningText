5671288600d0cd8b79665e0e7824686545c68fb2
Rajanuar Oky <rajanuar.oky@example.com>
2026-09-15 12:42:08 +0700

    fix: support legacy Android STB WebViews, remove sw.js, and prevent white screen

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    legacy({
+      targets: ['defaults', 'not IE 11', 'Android >= 5', 'Chrome >= 49'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+    }),
+  ],
 })
